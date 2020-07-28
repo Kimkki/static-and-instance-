276d6d738d4e87de788eed9b6b1bceb3c43888df
Felix kimutai <kmtflx@gmail.com>
2020-07-28 23:59:37 +0300

    initial commit

--- project1cl/Program.cs
+++ project1cl/Program.cs
@@ -6,10 +6,34 @@ using System.Threading.Tasks;
 
 namespace project1cl
 {
+    class Circle
+    {
+        static float _PI;
+        int Radius;
+
+        public Circle(int radius)
+        {
+            this.Radius = radius;
+        }
+
+        static Circle()
+        {
+            Circle._PI = 3.145F;
+        }
+
+        public float CalculateArea()
+        {
+            return Circle._PI * this.Radius * this.Radius;
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
+            Circle c1 = new Circle(5);
+            float area = c1.CalculateArea();
+
+            Console.WriteLine("Area = {0} ", area);
         }
     }
 }
